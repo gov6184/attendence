3ff12844f28a1184bbf87736182cfb5806a0f3a2
gov6184 <gm040381@gmail.com>
2024-03-31 17:47:04 +0530

    f

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,9 @@
 const exp=require("express")
 let {attendenceroute}=require("./route/attendenceroute")
 const bodyParser = require('body-parser');
+const cors = require("cors");
 const app=exp();
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Allow requests from your frontend origin
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allowed HTTP methods
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
-    next();
-  });
+app.use(cors())
   app.use(bodyParser.json({ limit: '1mb' }));
   app.use(exp.json());
 
