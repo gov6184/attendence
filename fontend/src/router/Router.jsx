5d34171ced0019a32d72f51de54e699ccd31ebcf
Govind Mishra <99667341+gov6184@users.noreply.github.com>
2024-03-31 17:10:38 +0530

    Update Router.jsx

--- fontend/src/router/Router.jsx
+++ fontend/src/router/Router.jsx
@@ -6,8 +6,8 @@ import { Route, Routes } from 'react-router-dom'
 export default function Router() {
   return (
     <Routes>
-{/*     <Route path='/' element={<Camera/>}  /> */}
-    <Route path='/' element={<AdminPage/>} />
+    <Route path='/' element={<Camera/>}  />
+    <Route path='/admin' element={<AdminPage/>} />
    
 </Routes>  )
 }
